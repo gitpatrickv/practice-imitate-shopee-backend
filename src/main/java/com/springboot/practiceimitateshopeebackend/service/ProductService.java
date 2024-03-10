b6e6377a505fa10a587e19a4c629554fb660cb41
rektgit2024 <myemailispatrick@gmail.com>
2024-03-11 00:08:58 +0800

    commit

--- src/main/java/com/springboot/practiceimitateshopeebackend/service/ProductService.java
+++ src/main/java/com/springboot/practiceimitateshopeebackend/service/ProductService.java
@@ -1,8 +1,6 @@
 package com.springboot.practiceimitateshopeebackend.service;
 
-import com.springboot.practiceimitateshopeebackend.entity.Product;
 import com.springboot.practiceimitateshopeebackend.model.ProductModel;
-import com.springboot.practiceimitateshopeebackend.model.Response;
 
 import java.util.List;
 import java.util.Optional;
