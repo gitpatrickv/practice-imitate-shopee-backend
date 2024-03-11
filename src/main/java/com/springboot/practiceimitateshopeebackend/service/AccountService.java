0402876c1556f3db290409dbfb6aa8668e80f9e4
rektgit2024 <myemailispatrick@gmail.com>
2024-03-11 23:24:41 +0800

    commit

--- src/main/java/com/springboot/practiceimitateshopeebackend/service/AccountService.java
+++ src/main/java/com/springboot/practiceimitateshopeebackend/service/AccountService.java
@@ -1,4 +1,13 @@
 package com.springboot.practiceimitateshopeebackend.service;
 
+import com.springboot.practiceimitateshopeebackend.model.ChangePasswordRequest;
+import com.springboot.practiceimitateshopeebackend.model.UpdateUserRequest;
+
+import java.security.Principal;
+
 public interface AccountService {
+
+    void changePassword(ChangePasswordRequest request, Principal user);
+
+    UpdateUserRequest updateUserInfo(UpdateUserRequest request);
 }
