ae7c2f125988480c3e85c1f8257eb09d8055a221
rektgit2024 <myemailispatrick@gmail.com>
2024-03-11 23:22:55 +0800

    commit

---
+++ src/main/java/com/springboot/practiceimitateshopeebackend/model/UpdateUserRequest.java
@@ -0,0 +1,17 @@
+package com.springboot.practiceimitateshopeebackend.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class UpdateUserRequest {
+    private String email;
+    private String name;
+    private String address;
+    private String contactNumber;
+}
