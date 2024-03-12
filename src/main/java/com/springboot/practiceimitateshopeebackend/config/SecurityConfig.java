abba550908fc82b1f86af62aed13fe1f4a8cb84c
rektgit2024 <myemailispatrick@gmail.com>
2024-03-12 22:57:10 +0800

    commit

--- src/main/java/com/springboot/practiceimitateshopeebackend/config/SecurityConfig.java
+++ src/main/java/com/springboot/practiceimitateshopeebackend/config/SecurityConfig.java
@@ -1,9 +1,11 @@
 package com.springboot.practiceimitateshopeebackend.config;
 
+import com.springboot.practiceimitateshopeebackend.security.JwtAuthenticationFilter;
 import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -12,10 +14,12 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @EnableWebSecurity
+@EnableMethodSecurity
 @Configuration
 @AllArgsConstructor
 public class SecurityConfig {
 
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
     private final AuthenticationProvider authenticationProvider;
 
     @Bean
@@ -23,10 +27,12 @@ public class SecurityConfig {
         httpSecurity.csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(authorize ->
                     authorize
-                            .requestMatchers("/api/**").permitAll()
-                            .anyRequest().permitAll()
+                            .requestMatchers("/api/user/**").permitAll()
+                            .anyRequest().authenticated()
                 );
         httpSecurity.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        httpSecurity.authenticationProvider(authenticationProvider);
+        httpSecurity.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
         return httpSecurity.build();
     }
 
