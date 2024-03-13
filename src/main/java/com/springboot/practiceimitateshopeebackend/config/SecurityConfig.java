f6a5f2203c10e5df389a8fbcca893d477ebcea83
rektgit2024 <myemailispatrick@gmail.com>
2024-03-13 19:35:24 +0800

    commit

--- src/main/java/com/springboot/practiceimitateshopeebackend/config/SecurityConfig.java
+++ src/main/java/com/springboot/practiceimitateshopeebackend/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import com.springboot.practiceimitateshopeebackend.security.JwtAuthenticationFil
 import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -12,6 +13,9 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import static com.springboot.practiceimitateshopeebackend.entity.constants.Role.ADMIN;
+import static com.springboot.practiceimitateshopeebackend.entity.constants.Role.SELLER;
 
 @EnableWebSecurity
 @EnableMethodSecurity
@@ -27,6 +31,12 @@ public class SecurityConfig {
         httpSecurity.csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(authorize ->
                     authorize
+                            .requestMatchers(HttpMethod.POST, "/api/product/save").hasAnyAuthority(ADMIN.name(), SELLER.name())
+                            .requestMatchers(HttpMethod.DELETE, "/api/product/delete/**").hasAnyAuthority(ADMIN.name(), SELLER.name())
+                            .requestMatchers("/api/cart/**").authenticated()
+                            .requestMatchers("/api/account/**").authenticated()
+                            .requestMatchers("/api/order/**").authenticated()
+                            .requestMatchers("/api/product/**").permitAll()
                             .requestMatchers("/api/user/**").permitAll()
                             .anyRequest().authenticated()
                 );
@@ -35,6 +45,5 @@ public class SecurityConfig {
         httpSecurity.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
         return httpSecurity.build();
     }
-
 
 }
--- src/main/java/com/springboot/practiceimitateshopeebackend/config/UserConfig.java
+++ src/main/java/com/springboot/practiceimitateshopeebackend/config/UserConfig.java
@@ -3,12 +3,11 @@ package com.springboot.practiceimitateshopeebackend.config;
 
 import com.springboot.practiceimitateshopeebackend.entity.User;
 import com.springboot.practiceimitateshopeebackend.entity.constants.Role;
-import com.springboot.practiceimitateshopeebackend.model.UserModel;
 import com.springboot.practiceimitateshopeebackend.repository.UserRepository;
-import com.springboot.practiceimitateshopeebackend.service.impl.UserServiceImpl;
 import com.springboot.practiceimitateshopeebackend.utils.StringUtils;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.hibernate.metamodel.internal.AbstractDynamicMapInstantiator;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -31,7 +30,7 @@ public class UserConfig {
                 user.setName("ADMIN");
                 user.setEmail("admin@gmail.com");
                 user.setPassword(passwordEncoder.encode("admin"));
-                user.setRole(Role.ADMIN);
+                user.setRole(Role.valueOf("ADMIN"));
                 userRepository.save(user);
                 log.info(StringUtils.ADMIN_CREATED);
             }
