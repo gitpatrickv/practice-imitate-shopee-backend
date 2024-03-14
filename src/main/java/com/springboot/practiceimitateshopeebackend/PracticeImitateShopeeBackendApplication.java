571c746f9c8898c6c34bd45a4827ef1daa1025dc
rektgit2024 <myemailispatrick@gmail.com>
2024-03-14 17:37:03 +0800

    commit

--- src/main/java/com/springboot/practiceimitateshopeebackend/PracticeImitateShopeeBackendApplication.java
+++ src/main/java/com/springboot/practiceimitateshopeebackend/PracticeImitateShopeeBackendApplication.java
@@ -13,24 +13,13 @@ public class PracticeImitateShopeeBackendApplication {
 	}
 
 
-	//todo place order
+
 	//todo select payment methods
 	//todo kafka to handle all events
 	//todo validation
 	//todo UNIT TEST
 
-	//todo show all my purchase history
-	//todo show all completed orders
-
-
-	//todo buy again
-
 	//todo flyway practice
 
-	//todo generate random order id
-	//todo payment method
 	//todo order status = COMPLETED CANCELLED PENDING PROCESSING, OUT FOR DELIVERY make a method for changing order status?
-	//todo transaction history different from order? when order is cancelled / completed it will go to transaction history db then be deleted in the order db?
-	//todo kafka to handle all events
-
 }
