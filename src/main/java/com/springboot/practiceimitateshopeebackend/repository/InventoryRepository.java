eca355ba68286c8398698ea5bb366d7e6a2714ad
rektgit2024 <myemailispatrick@gmail.com>
2024-03-28 18:04:30 +0800

    commit

--- src/main/java/com/springboot/practiceimitateshopeebackend/repository/InventoryRepository.java
+++ src/main/java/com/springboot/practiceimitateshopeebackend/repository/InventoryRepository.java
@@ -4,8 +4,6 @@ import com.springboot.practiceimitateshopeebackend.entity.Inventory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
 public interface InventoryRepository extends JpaRepository<Inventory, Long> {
 
